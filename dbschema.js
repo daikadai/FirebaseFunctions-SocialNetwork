ec94b8d95cba4b0e50ba160af68037f7355f8b93
Pham Cao Dai <daikadai1026@gmail.com>
2020-07-16 11:09:36 +0700

    2. Express & formatting response

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2020-07-16T04:03:09.292Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
--- functions/index.js
+++ functions/index.js
@@ -7,34 +7,32 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://socialape-be6ca.firebaseio.com"
 });
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello World!");
-});
 
-exports.getScreams = functions.https.onRequest((req,res) => {
-  admin.firestore().collection('screams').get()
+const express = require('express');
+const app = express();
+
+app.get('/screams', (req,res) => {
+  admin.firestore().collection('screams').orderBy('createdAt', 'desc').get()
     .then(data => {
       let screams = [];
       data.forEach(doc => {
-        screams.push(doc.data())
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        })
       })
       return res.json(screams);
     })
     .catch(err => console.error(err))
 })
 
-exports.createScream = functions.https.onRequest((req,res) => {
-  if(req.method !== 'POST') {
-    return res.status(400).json({ error: 'Method not allowed'});
-  }
+app.post('/scream', (req,res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   }
 
   admin.firestore()
@@ -48,3 +46,5 @@ exports.createScream = functions.https.onRequest((req,res) => {
       console.error(err);
     })
 })
+
+exports.api = functions.https.onRequest(app);
