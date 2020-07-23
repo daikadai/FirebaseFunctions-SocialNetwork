dba76d44b10ad48bcbd7a2cc4d112c631803e32f
Pham Cao Dai <daikadai1026@gmail.com>
2020-07-23 22:10:03 +0700

    Like, Unlike and Delete Scream

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream, getScream, commentOnScream } = require('./handlers/screams');
+const { getAllScreams, postOneScream, getScream, commentOnScream, likeScream , unlikeScream, deleteScream} = require('./handlers/screams');
 
 const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
 
@@ -13,8 +13,11 @@ app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
 // delete scream
+app.delete('/scream/:screamId', FBAuth, deleteScream);
 // like a scream
+app.get('/scream/:screamId/like', FBAuth, likeScream);
 // unlike a scream
+app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 // comment on scream
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream)
 
